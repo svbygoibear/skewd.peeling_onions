a9da2f627335e88771454bf212e5128458243c74
simoneslatnpou <svbyogibear@gmail.com>
2016-05-22 18:21:34 +0200

    Initial commit

---
+++ skewd.onion_test/TestLayers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using skewd.peeling_onions;
+
+namespace skewd.onion_test {
+    [TestClass]
+    public class TestLayers {
+        [TestMethod]
+        public void NumberOfLayers() {
+            var numberOfLayers = new Onion(new Onion(new Onion(new Onion()))).NumberOfInnerLayers;
+            var didItWork = false;
+
+            if (numberOfLayers == 3)
+                didItWork = true;
+
+            Assert.AreEqual(true, didItWork); //assert if result is as expected
+        }
+    }
+}
---
+++ skewd.peeling_onions/Onion.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace skewd.peeling_onions {
+    public class Onion {
+        private readonly Onion containedLayer;
+
+        public Onion() { }
+
+        public Onion(Onion containedLayer) {
+            this.containedLayer = containedLayer;
+        }
+
+        public int NumberOfInnerLayers {
+            get {
+                int count = 0;
+                Onion inner = this.containedLayer;
+
+                while (inner != null) {
+                    count++;
+                    inner = inner.containedLayer;
+                }
+                    
+                return count;
+            }
+        }
+    }
+}
